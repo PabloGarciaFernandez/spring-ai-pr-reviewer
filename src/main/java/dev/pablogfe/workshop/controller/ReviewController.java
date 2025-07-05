91453b7715fccfcacc7c23846e7dce8bcd7ddd05
Pablo García Fernández <uo276824@uniovi.es>
2025-07-05 13:58:21 +0200

    Update

--- src/main/java/dev/pablogfe/workshop/controller/ReviewController.java
+++ src/main/java/dev/pablogfe/workshop/controller/ReviewController.java
@@ -2,6 +2,7 @@ package dev.pablogfe.workshop.controller;
 
 import dev.pablogfe.workshop.service.ReviewService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
